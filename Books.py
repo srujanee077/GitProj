37c19ad36296ff38b01fa895567b4bee855f7b7d
Srujanee <srujanee077@gmail.com>
2020-04-19 15:20:02 +0530

    commented nested function

--- Books.py
+++ Books.py
@@ -35,27 +35,32 @@ print(my_list)
 def csv_fn(file):
     df = pd.read_csv(file)
     pd.options.display.max_columns = len(df.columns)
-    print(df)
-    return df.values.tolist()
+    print(df.head())
+    #df.style.set_properties(**{'text-align': 'left'})
+    #print(df)
+    return df.T.to_dict()
 
 
 # def nested(isbn,my_list):
 #     return any(isbn in nested for nested in my_list)
 
-def search(my_list,isbn):
+def search(my_list,valueToSearch,keyTosearch):
     exp_List=[]
-    for k in my_list:
-        for v in k:
-            if str(isbn) in str(v) :
-                #print(k,'found')
-                exp_List = k
-                break
+    for k,i in my_list.items():
+       for key,value in i.items():
+           if str(keyTosearch) in str(key):
+               if str(valueToSearch) in str(value):
+                 exp_List =exp_List+list(i.items())
+
     return exp_List
 
 df=csv_fn(file)
-isbn = int(input("Enter the ISBN No to be searched : "))
+#print(df)
+key = input("Enter key to serach --options/--BookName/Author/Publisher/Price/Date of Publication/ISBN Code")
+value = input("Enter the Value to be searched : ")
+
 #print(nested(isbn,df),'List Books')
-print('Matched Book List--',search(df,isbn))
+print('Matched Book List--',search(df,value,key))
 
 
 
