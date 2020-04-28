1df32f608f57deea84a4af349c609770daa42077
Srujanee <srujanee077@gmail.com>
2020-04-28 21:53:40 +0530

    second commit

--- app.py
+++ app.py
@@ -1,50 +1,23 @@
-from flask import Flask, request, jsonify
-app = Flask(__name__)
+import flask
+from flask import jsonify
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
-    # For debugging
-    print(f"got name {name}")
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    ]
 
-    response = {}
+@app.route('/mylibrary/books', methods=['GET'])
+def home():
+    return jsonify("Hello from Books")
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+# @app.route('/books', methods=['GET'])
+# def api_all():
+#     return jsonify(books)
 
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+app.run()
