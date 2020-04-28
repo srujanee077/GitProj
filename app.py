c676a11528fd0d3ac943cdbdf80c6ba6ac856c29
Srujanee <srujanee077@gmail.com>
2020-04-28 22:02:46 +0530

    second commit

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-import flask
-from flask import jsonify
+from flask import Flask, request, jsonify
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    ]
-
-@app.route('/mylibrary/books', methods=['GET'])
-def home():
-    return jsonify("Hello from Books")
-
-# @app.route('/books', methods=['GET'])
-# def api_all():
-#     return jsonify(books)
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
 
 app.run()
