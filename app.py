e0a8ff774a670d572236e2959ff04423d4235003
Srujanee <srujanee077@gmail.com>
2020-04-28 23:03:39 +0530

    second commit

--- app.py
+++ app.py
@@ -13,14 +13,17 @@ books = [
     ]
 @app.route('/')
 def index():
-    return "<h1>Welcome to our Library Management!!</h1>"
+    return "<h1>Welcome to our server !!</h1>"
 
-@app.route('/mylibrary/books', methods=['GET'])
-def home():
-    return jsonify("Hello from Books")
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
+
+# @app.route('/mylibrary/books', methods=['GET'])
+# def home():
+#     return jsonify("Hello from Books")
 
 # @app.route('/books', methods=['GET'])
 # def api_all():
 #     return jsonify(books)
 
-app.run()
