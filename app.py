38a4dbd2643e8632c0226bdb450ccf7e151b50e7
Srujanee <srujanee077@gmail.com>
2020-04-30 21:12:44 +0530

    third commit

--- app.py
+++ app.py
@@ -7,19 +7,17 @@ app.config["DEBUG"] = True
 
 @app.route('/mylibrary/books/', methods=['GET'])
 def respond():
-    return jsonify("Hello from Books")
+    return jsonify("Hello from Books version 2")
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to our Library MAnagement !!</h1>"
+    return "<h1>Welcome to our Library Management !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
 
-@app.route('/mylibrary/books', methods=['GET'])
-def home():
-    return jsonify("Hello from Books")
+
 
 # @app.route('/books', methods=['GET'])
 # def api_all():
