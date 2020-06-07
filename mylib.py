c1aba5b3e54eeefb34210a290c5b0a767cd12310
Srujanee <srujanee077@gmail.com>
2020-06-08 00:40:29 +0530

    lib API function

--- mylib.py
+++ mylib.py
@@ -11,6 +11,46 @@ books = [
      'first_sentence': 'The coldsleep itself was dreamless.',
      'year_published': '1992'},
     ]
+[{
+         'name':'Harry Potter',
+        'author':'J.K.Rowling',
+        'publisher':'Bloomsbury',
+        'price':600,
+        'published':2000,
+        'ISBN Code':446310785,
+              },
+      {
+         'name':'To Kill a Mockingbird',
+        'author':'Harper Lee Read',
+        'publisher':'J. B. Lippincott & Co.',
+        'price':500,
+        'published':1960,
+        'ISBN Code':446310786},
+
+     {
+        'name':'A Fire Upon the Deep',
+        'author': 'Vernor Vinge',
+        'publisher': 'J. B. Lippincott & Co.',
+        'price': 500,
+        'published': 1992,
+        'ISBN Code':446310788},
+    {
+        'name':'Ancillary Justice',
+        'author': 'Ann Leckie',
+        'publisher': 'Dzanc Books (Detroit, MI)',
+        'price': 800,
+        'published': 2014,
+        'ISBN Code':446310710
+    },
+  {
+    'name':'The Yiddish Policemen Union1',
+    'author': 'Michael Chabon',
+    'publisher': 'Rare Bird Books (Los Angeles, CA)',
+    'price': 300,
+    'published': 2008,
+    'ISBN Code': 446310717
+  }
+]
 
 @app.route('/mylibrary/books', methods=['GET'])
 def home():
